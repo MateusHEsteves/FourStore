731387ebd8675d76267c787fc33620d202ede1ad
Mateus Esteves <mateus-esteves@hotmail.com>
2022-05-25 21:59:06 -0300

    metodo de compras parcialmente implementado

--- src/br/com/fourstore/controller/MenuController.java
+++ src/br/com/fourstore/controller/MenuController.java
@@ -1,6 +1,8 @@
 package br.com.fourstore.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 import br.com.fourstore.model.Produto;
@@ -13,7 +15,7 @@ public class MenuController {
 	ProdutoService productService = new ProdutoService();
 
 	public MenuController(String opcao) {
-		System.out.println("Programa foi inicializado pela " + opcao + "ª opção");
+		System.out.println("Programa foi inicializado pela " + opcao + "Âª opÃ§Ã£o");
 		inicializeMenu();
 	}
 
@@ -26,7 +28,7 @@ public class MenuController {
 			case 1:
 				cadastrarProduto();
 				continue;
-				
+
 			case 2:
 				updateEstoque();
 				continue;
@@ -44,7 +46,7 @@ public class MenuController {
 	 * Exibe um menu no console e solicita uma entrada do tipo inteiro usando o
 	 * Scanner e retorna essa entrada.
 	 * 
-	 * @return Integer inputado pelo usuário do sistema.
+	 * @return Integer inputado pelo usuÃ¡rio do sistema.
 	 */
 	public Integer menuPrincipal() {
 		System.out.println("╔══════════════════════════════════════════╗");
@@ -71,12 +73,12 @@ public class MenuController {
 
 		Integer sku = Integer.parseInt(readString("Informe o Sku: "));
 		String nome = readString("Informe o nome: ");
-		Double preco = Double.parseDouble(readString("Informe o preço: "));
+		Double preco = Double.parseDouble(readString("Informe o preÃ§o: "));
 		String tamanho = readString("Informe o tamanho: ");
 		String cor = readString("Informe a cor: ");
 		Integer quantidade = Integer.parseInt(readString("Informe a quantidade: "));
 		String tipo = readString("Informe o tipo: ");
-		String descricao = readString("Informe a descrição: ");
+		String descricao = readString("Informe a descriÃ§Ã£o: ");
 		String categoria = readString("Informe a categoria: ");
 		String departamento = readString("Informe o departamento: ");
 
@@ -97,20 +99,136 @@ public class MenuController {
 		List<Produto> estoqueList = productService.readAll();
 		for (Produto produto : estoqueList) {
 			System.out.println(produto);
-		}	
+		}
 		return productService.readAll();
 	}
-	
+
 	public void updateEstoque() {
 		System.out.println("Informe o sku do produto a ser alterado: ");
 		Integer sku = scanner.nextInt();
-		scanner.nextLine();	
+		scanner.nextLine();
 		Produto produtoASerAlterado = productService.readBySku(sku);
 		System.out.println("Informe a quantidade do produto a ser alterado: ");
 		Integer quantidade = (produtoASerAlterado.getQuantidade() + scanner.nextInt());
-		scanner.nextLine();		
+		scanner.nextLine();
 		produtoASerAlterado.setQuantidade(quantidade);
 		productService.update(produtoASerAlterado);
 		System.out.println("Produto atualizado com sucesso! ");
+	}
+
+	/*
+	 * Criar um carrinho de compras Forma de pagamento perguntar se deseja cpf na
+	 * nota recibo de pagamento
+	 */
+	private void sellProduct() {
+
+		Map<Produto, Integer> cart = new HashMap<>();
+
+		Integer option = 1;
+
+		while (option == 1) {
+
+			System.out.println("Informe o SKU do produto a ser vendido: ");
+			Integer sku = scanner.nextInt();
+			System.out.println("Quantidade de produto a ser vendido: ");
+			Integer quantity = scanner.nextInt();
+
+			System.out.println("Deseja comprar mais produtos? 1 - sim / 2 - Não.");
+			option = scanner.nextInt();
+
+			cart.put(productService.readBySku(sku), quantity);
+
+		}
+
+		System.out.println("╔══════════════════════════════════════════╗");
+		System.out.println("║          × Metodo de pagamento ×         ║");
+		System.out.println("╠══════════════════════════════════════════╣");
+		System.out.println("║ » 1 - Money                              ║");
+		System.out.println("║ » 2 - Debit card                         ║");
+		System.out.println("║ » 3 - Credit card                        ║");
+		System.out.println("║ » 4 - Pix                                ║");
+		System.out.println("╚══════════════════════════════════════════╝");
+		System.out.print("  » ");
+		Integer metodoDePagamento = scanner.nextInt();
+
+		switch (metodoDePagamento) {
+		case 1:
+			System.out.println("Pagamento no dinheiro realizado com sucesso!");
+			break;
+		case 2:
+			while (true) {
+
+				System.out.println("Informe o nome do titular: ");
+				String titularName = scanner.nextLine();
+				System.out.println("Informe o número do cartão: ");
+				Integer numberCard = scanner.nextInt();
+				System.out.println("Informe o número do cvv: ");
+				Integer numberCvv = scanner.nextInt();
+				System.out.println("Nome: " + titularName + ", " + numberCard + ", " + numberCvv + ".");
+				System.out.println("Os dados acima estão corretos? 1 - Sim / 2 - Não.");
+				option = scanner.nextInt();
+				if (option == 2) {
+					continue;
+				}
+				break;
+			}
+
+			System.out.println("Pagamento no débito realizado com sucesso!");
+
+			break;
+		case 3:
+
+			while (true) {
+
+				System.out.println("Informe o nome do titular: ");
+				String titularName = scanner.nextLine();
+				System.out.println("Informe o número do cartão: ");
+				Integer numberCard = scanner.nextInt();
+				System.out.println("Informe o número do cvv: ");
+				Integer numberCvv = scanner.nextInt();
+				System.out.println("Nome: " + titularName + ", " + numberCard + ", " + numberCvv + ".");
+				System.out.println("Os dados acima estão corretos? 1 - Sim / 2 - Não.");
+				option = scanner.nextInt();
+				if (option == 2) {
+					continue;
+				}
+				break;
+			}
+
+			System.out.println("Pagamento no crédito realizado com sucesso!");
+		case 4:
+
+			System.out.println("█▀▀▀▀▀█ ▀▀    ▀ █ █▀▀▀▀▀█");
+			System.out.println("█ ███ █ █▄ █▀█▄▀  █ ███ █");
+			System.out.println("█ ▀▀▀ █ ▀█▀▀▄██▀█ █ ▀▀▀ █");
+			System.out.println("▀▀▀▀▀▀▀ ▀▄▀▄▀ ▀ ▀ ▀▀▀▀▀▀▀");
+			System.out.println("▀▀▀▀▄ ▀▄█▄    ▀▀██ ▄▀▀▀▄▀");
+			System.out.println(" ▀█ ▀▄▀█   ▄▄▄█ ▄  █ ▄▄▄ ");
+			System.out.println("  █  ▄▀█▀█▄█▀██▀▄█████▀▄█");
+			System.out.println("▄▀ ██ ▀█▄█  ▀▀▄  █▄▄ ▀▀█▄");
+			System.out.println("   ▀ ▀▀ ▄▄▄▄▄▀ ▄█▀▀▀███ █");
+			System.out.println("█▀▀▀▀▀█  ▄▀ ▄ █▄█ ▀ █▄ ██");
+			System.out.println("█ ███ █ ▄▀ █ ▄  ▀██▀██ ▄█");
+			System.out.println("█ ▀▀▀ █ ██ █▀█ ▀ ▄▀▀▀▀█▀ ");
+			System.out.println("▀▀▀▀▀▀▀ ▀ ▀ ▀▀▀    ▀▀▀▀▀▀");
+			
+			System.out.println("Confirmando pagamento PIX");
+			try {
+			Thread.sleep(500);
+			System.out.print(".");
+			Thread.sleep(500);
+			System.out.print(".");
+			Thread.sleep(500);
+			System.out.print(".");
+			} catch (Exception e) {
+				
+			}
+			
+			System.out.println("Pagamento no PIX realizado com sucesso!");
+
+		default:
+			break;
+		}
+
 	}
 }
